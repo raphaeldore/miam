a29d84ba1be682d134cb3e15d6e2efec929682a5
Yannick Mazières <yannickmazieres@yahoo.ca>
2014-08-26 14:34:17 -0400

    mise à jour des packages EF et EfCompact. Supression du package EF dans app.web

---
+++ App/Miam.DataLayer/EfDatabaseConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.SqlServerCompact;
+
+namespace Miam.DataLayer
+{
+    public class EfDatabaseConfiguration : DbConfiguration
+    {
+        public EfDatabaseConfiguration()
+        {
+            SetProviderServices(SqlCeProviderServices.ProviderInvariantName,
+                                SqlCeProviderServices.Instance);
+            
+        }
+    }
+}
+
+// Pour l'explication concernant DbConfiguration voir:
+// http://msdn.microsoft.com/en-us/library/system.data.entity.dbconfiguration(v=vs.113).aspx 
+// http://msdn.microsoft.com/en-US/data/jj680699
+//
+// Configuration for an Entity Framework application can be specified in a config file (app.config/web.config) or through code.
+// The latter is known as code-based configuration. 
+// Configuration in a config file is described in a separate article. 
+// The config file takes precedence over code-based configuration. 
+// In other words, if a configuration option is set in both code and in the config file, 
+// then the setting in the config file is used.
