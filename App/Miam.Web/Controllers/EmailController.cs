69d976f3d43f27f74225cfc47d7cd024e0bbcf53
Yannick Mazières <yannickmazieres@yahoo.ca>
2014-10-24 11:16:49 -0400

    utilisation de jenkins.cegep-ste-foy.qc.ca

--- App/Miam.Web/Controllers/EmailController.cs
+++ App/Miam.Web/Controllers/EmailController.cs
@@ -11,11 +11,14 @@ namespace Miam.Web.Controllers
     public partial class EmailController : Controller
     {
         //Code incomplet, qui a pour but de montrer l'utilisation de smtp4dev.
-        // - smtp4dev permet d'envoyer et recevoir des courriels sur son poste de travail sans avoir a configuer un serveur SMTP
-        // - Les tests d'acceptation executés en local utilisent smtp4dev.
-        // - Les tests d'acceptation executés sur le serveur d'integration (Jenkins) utilisent un vrai serveur SMTP.
+        // - smtp4dev permet d'envoyer et recevoir des courriels sur son poste de travail sans avoir à configurer un serveur SMTP
+        // - Les tests d'acceptation exécutés en local utilisent smtp4dev.
+        // - Les tests d'acceptation exécutés sur le serveur d'intégration (Jenkins) utilisent un vrai serveur SMTP.
         //
         //Pour télécharger smpt4dev: https://smtp4dev.codeplex.com/
+        //
+        //N'oublier pas d'ajouter sur votre poste la ligne ci-dessous dans C:\Windows\System32\drivers\etc\hosts
+        //127.0.0.1 jenkins.cegep-ste-foy.qc.ca
 
         [HttpGet]
         public virtual ActionResult Send()
@@ -32,7 +35,7 @@ namespace Miam.Web.Controllers
             client.Port = 25;
             client.DeliveryMethod = SmtpDeliveryMethod.Network;
             client.UseDefaultCredentials = false;
-            client.Host = "localhost";
+            client.Host = "jenkins.cegep-ste-foy.qc.ca";
             mail.Subject = sendEmailViewModel.Subject;
             mail.Body = sendEmailViewModel.Body;
             client.Send(mail);
