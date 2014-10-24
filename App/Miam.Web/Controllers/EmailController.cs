837c4ffee678c56fbaa46913d90edb1a51cba8c4
ymazieres <yannickmazieres@yahoo.ca>
2014-10-24 12:40:31 -0400

    Update EmailController.cs

--- App/Miam.Web/Controllers/EmailController.cs
+++ App/Miam.Web/Controllers/EmailController.cs
@@ -10,17 +10,18 @@ namespace Miam.Web.Controllers
 {
     public partial class EmailController : Controller
     {
-        //Code incomplet, qui a pour but de montrer l'utilisation de smtp4dev.
-        // - smtp4dev permet d'envoyer et recevoir des courriels en local (évite d'envoyer pour vrai les courriels lors du développement).
-        // - Les tests d'acceptation exécutés en local utilisent smtp4dev. Aucun courriel ne sera envoyé.
-        // - Les tests d'acceptation exécutés sur le serveur d'intégration (Jenkins) utilisent un vrai serveur SMTP. Les courriels sont réellement envoyés.
+        //Code incomplet, qui a pour but de montrer l'utilisation du serveur smtp de jenkins.
+        // Lors du développement et pour tester en local, il est conseillé de ne pas envoyer les courriels au serveur smpt de jenkins. 
+        // Pour y arriver, il faut installer un serveur smpt en local (smtp4dev)
+        // - Les tests d'acceptation exécutés en local utiliseront smtp4dev. Aucun courriel ne sera vraiment envoyé.
+        // - Les tests d'acceptation exécutés sur le serveur d'intégration (Jenkins) utiliseront un vrai serveur SMTP. Les courriels sont réellement envoyés.
         //
         //Pour télécharger smpt4dev: https://smtp4dev.codeplex.com/
         //
         //Configuration en local: 
-        //  Installer et exécuter smtp4dev
-        //  Ajouter la ligne ci-dessous dans le fichier hosts  (C:\Windows\System32\drivers\etc\hosts):
-        //  127.0.0.1 jenkinssmtp.cegep-ste-foy.qc.ca
+        //  - Installer et exécuter smtp4dev
+        //  - Ajouter la ligne ci-dessous dans le fichier hosts  (C:\Windows\System32\drivers\etc\hosts):
+        //    127.0.0.1 jenkinssmtp.cegep-ste-foy.qc.ca
 
         [HttpGet]
         public virtual ActionResult Send()
