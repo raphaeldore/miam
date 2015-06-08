612f4285913a007a30353e2139cb09b1c04f642a
Yannick Mazières <yannickmazieres@yahoo.ca>
2015-06-08 09:23:30 -0400

    Utilisation d'un profile pour firefox

--- Tests/Miam.Web.Automation/Seleno/Host.cs
+++ Tests/Miam.Web.Automation/Seleno/Host.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Windows;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using TestStack.Seleno.Configuration;
 using TestStack.Seleno.Configuration.WebServers;
 using TestStack.Seleno.Extensions;
@@ -14,34 +15,36 @@ namespace Miam.Web.Automation.Seleno
     {
         public static readonly SelenoHost Instance = new SelenoHost();
 
+       
         static Host()
         {
-            //Todo: utiliser les profiles
-            //var fireFoxSeleniumProfile = CreateSeleniumPorfile(); 
-            //Instance2 = new FirefoxDriver(fireFoxSeleniumProfile);
-
-            
-
             Instance.Run(configure => configure
                 .WithWebServer(new InternetWebServer("http://miam.local"))
                 .WithRouteConfig(RouteConfig.RegisterRoutes)
                 .WithMinimumWaitTimeoutOf(TimeSpan.FromSeconds(5))
-                //.WithRemoteWebDriver()
+                .WithRemoteWebDriver(fireFoxWebDriver)
             );
+
             Instance.Application.Browser.Manage().Window.Maximize();
         }
 
-        //public static FirefoxProfile CreateSeleniumPorfile()
-        //{
-        //    // Configurer FireFox pour ne pas ouvrir de boite de dialogue si un utilisateur demande de télécharger un fichier.
-        //    var profile = new FirefoxProfile();
-        //    var applicationDirectory = Directory.GetCurrentDirectory();
-        //    profile.SetPreference("browser.download.dir", applicationDirectory);
-        //    profile.SetPreference("browser.download.folderList", 2);
-        //    profile.SetPreference("browser.helperApps.alwaysAsk.force", false);
-        //    profile.SetPreference("browser.helperApps.neverAsk.saveToDisk", "application/zip, application/x-zip, application/x-zip-compressed, application/download, application/octet-stream");
+        static Func<RemoteWebDriver> fireFoxWebDriver = delegate()
+        {
+            var fireFoxSeleniumProfile = CreateSeleniumPorfile();
+            return new FirefoxDriver(fireFoxSeleniumProfile);
+        };
+    
+        public static FirefoxProfile CreateSeleniumPorfile()
+        {
+            // Configurer FireFox pour ne pas ouvrir de boite de dialogue si un utilisateur demande de télécharger un fichier.
+            var profile = new FirefoxProfile();
+            var applicationDirectory = Directory.GetCurrentDirectory();
+            profile.SetPreference("browser.download.dir", applicationDirectory);
+            profile.SetPreference("browser.download.folderList", 2);
+            profile.SetPreference("browser.helperApps.alwaysAsk.force", false);
+            profile.SetPreference("browser.helperApps.neverAsk.saveToDisk", "application/zip, application/x-zip, application/x-zip-compressed, application/download, application/octet-stream");
 
-        //    return profile;
-        //}
+            return profile;
+        }
     }
 }
