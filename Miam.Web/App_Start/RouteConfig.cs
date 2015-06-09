c448134a7525cb3e85507dd72a02f49502d571da
Yannick Mazières <yannickmazieres@yahoo.ca>
2015-06-09 06:51:46 -0400

    modif build.xml + fichier de config

--- Miam.Web/App_Start/RouteConfig.cs
+++ Miam.Web/App_Start/RouteConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -12,6 +13,11 @@ namespace WebApplication1
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            //if (ConfigurationManager.AppSettings["environment"] == "Production")
+            //{
+            //    routes.IgnoreRoute("Ci");
+            //}
 
             routes.MapRoute(
                 name: "Default",
