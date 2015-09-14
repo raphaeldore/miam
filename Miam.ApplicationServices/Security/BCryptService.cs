460460964be63f54cf06c7ab030e0ac140c24b70
Raphaël Doré <rdore@neomailbox.ch>
2015-09-14 13:23:33 -0400

    Création des classes IHashService et BCryptService.
    Ces classes serviront principalement à calculer le hash d'un mot de passe,
    et de valider le mot de passe d'un usager.

---
+++ Miam.ApplicationServices/Security/BCryptService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miam.ApplicationServices.Security
+{
+    public class BCryptService : IHashService
+    {
+        private const int BCRYPT_WORK_FACTOR = 12;
+
+        public string Hash(string value)
+        {
+            return BCrypt.Net.BCrypt.HashString(value, BCRYPT_WORK_FACTOR);
+        }
+
+        public string HashPassword(string value)
+        {
+            string salt = BCrypt.Net.BCrypt.GenerateSalt(BCRYPT_WORK_FACTOR);
+
+            return BCrypt.Net.BCrypt.HashPassword(value, salt);
+        }
+
+        public bool Verify(string value, string hash)
+        {
+            if (value == null)
+                return false;
+
+            return BCrypt.Net.BCrypt.Verify(value, hash);
+        }
+
+        public bool VerifyPassword(string value, string passhash)
+        {
+            if (value == null)
+                return false;
+
+            return BCrypt.Net.BCrypt.Verify(value, passhash);
+        }
+    }
+}
---
+++ Miam.ApplicationServices/Security/IHashService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miam.ApplicationServices.Security
+{
+    public interface IHashService
+    {
+        String Hash(String value);
+        String HashPassword(String value);
+        Boolean Verify(String value, String hash);
+        Boolean VerifyPassword(String value, String passhash);
+    }
+}
